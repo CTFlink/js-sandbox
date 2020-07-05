c14c87e10f8611031e9893281176a0eaede0842d
Christian Flink <ctflink@gmail.com>
2020-07-05 23:04:10 +0200

    result

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ["B", "B", "B", "B"];
 const form = document.querySelector(".quiz-form");
+const result = document.querySelector(".result");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -18,6 +19,8 @@ form.addEventListener("submit", (e) => {
       score += 25;
     }
   });
+  //Show result on page
 
-  console.log(score);
+  result.querySelector("span").textContent = `${score}%`;
+  result.classList.remove("d-none");
 });
